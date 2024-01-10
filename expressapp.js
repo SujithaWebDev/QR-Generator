e6d28d86656a96b646fac24b1907a16b0b98de6e
Sujitha <sujithawebdev@gmail.com>
2024-01-10 15:41:58 +0530

    favicon added

--- expressapp.js
+++ expressapp.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({extended:true}))
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"))
 app.use(express.static('public'))
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.get("/",(req,res)=>{
     res.render('index')
@@ -21,4 +22,4 @@ app.post("/yourqr",(req,res)=>{
         res.render('generate',{sujitha:data})
     })
 })
-app.listen(4001)
+app.listen(4001);
